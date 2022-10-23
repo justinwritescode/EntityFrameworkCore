bf97ee7fe8d6546daf04848e49d6bc199db30987
Justin <justin@thebackroom.app>
2022-10-23 17:47:12 -0400

    Checking in so I can work on the VM...

--- src/Extensions/IdValueGenerator.cs
+++ src/Extensions/IdValueGenerator.cs
@@ -1,4 +1,5 @@
 namespace JustinWritesCode.EntityFrameworkCore.Extensions;
+using static JustinWritesCode.Types.Extensions.RandomV2;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.ValueGeneration;
 
@@ -12,14 +13,14 @@ public class IdValueGenerator : ValueGenerator
         var _type = idProperty.Metadata.ClrType;
         return _type.Name switch
         {
-            nameof(System.Byte) => RandomByte(),
-            nameof(System.SByte) => RandomSByte(),
-            nameof(System.Int16) => RandomLong(1000, 10000000),
-            nameof(System.UInt16) => RandomULong(1000, 10000000),
-            nameof(System.Int32) => RandomInt(1000, 10000000),
-            nameof(System.UInt32) => RandomUInt(1000, 10000000),
-            nameof(System.Int64) => RandomLong(1000, 10000000),
-            nameof(System.UInt64) => RandomULong(1000, 10000000),
+            nameof(System.Byte) => RandomUInt8(),
+            nameof(System.SByte) => RandomInt8(),
+            nameof(System.Int16) => RandomInt16(1000, 10000000),
+            nameof(System.UInt16) => RandomUInt16(1000, 10000000),
+            nameof(System.Int32) => RandomInt32(1000, 10000000),
+            nameof(System.UInt32) => RandomUInt32(1000, 10000000),
+            nameof(System.Int64) => RandomInt64(1000, 10000000),
+            nameof(System.UInt64) => RandomUInt64(1000, 10000000),
             nameof(System.String) => Guid.NewGuid().ToString(),
             _ => Guid.NewGuid()
         };
