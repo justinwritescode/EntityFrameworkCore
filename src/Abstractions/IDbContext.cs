3d4818246df004ce730692e4ed96a3f3281e421f
David <justin@justinwritescode.com>
2022-11-01 15:56:56 -0400

    Update IDbContext.cs

--- src/Abstractions/IDbContext.cs
+++ src/Abstractions/IDbContext.cs
@@ -1,10 +1,23 @@
+// 
+// IDbContext.cs
+// 
+//   Created: 2022-10-18-07:51:12
+//   Modified: 2022-10-30-04:02:42
+// 
+//   Author: Justin Chase <justin@justinwritescode.com>
+//   
+//   Copyright © 2022 Justin Chase, All Rights Reserved
+//      License: MIT (https://opensource.org/licenses/MIT)
+// 
+
 namespace JustinWritesCode.EntityFrameworkCore.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 
 /// <summary>
 /// Interface for a database context.  Implements all public properties and methods of <see cref="DbContext"/>.
-/// </summary>[ProxyInterfaceGenerator.Proxy(typeof(DbContext))]
+/// </summary>
+[ProxyInterfaceGenerator.Proxy(typeof(DbContext))]
 public partial interface IDbContext
 {
 }
@@ -12,6 +25,6 @@ public partial interface IDbContext
 /// <summary>
 /// Interface for a database context.  Implements all public properties and methods of <see cref="DbContext"/>.
 /// </summary>
-public interface IDbContext<TContext> : IDbContext where TContext : DbContext
+public interface IDbContext<TSelf> : IDbContext where TSelf : DbContext
 {
 }
