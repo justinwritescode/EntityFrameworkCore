c8dbafecad5c7169694d0c67775ef9ab609c16b4
Justin <justin@justinwritescode.com>
2023-01-08 21:37:08 -0600

    Update ITimestampedEntity{TId,TUserId}.cs

--- src/Abstractions/IEntity/ITimestampedEntity{TId,TUserId}.cs
+++ src/Abstractions/IEntity/ITimestampedEntity{TId,TUserId}.cs
@@ -9,20 +9,18 @@
  *   Copyright © 2022-2023 Justin Chase, All Rights Reserved
  *      License: MIT (https://opensource.org/licenses/MIT)
  */
-namespace JustinWritesCode.EntityFrameworkCore.Abstractions
+namespace JustinWritesCode.EntityFrameworkCore.Abstractions;
+/// <summary>
+/// <inheritdoc cref="ITimestampedEntity" />
+/// </summary>
+/// <typeparam name="TId"><inheritdoc cref="ITimestampedEntity{TId}"/></typeparam>
+/// <typeparam name="TUserId">The type of the user ID</typeparam>
+public interface ITimestampedEntity<TId, TUserId> : ITimestampedEntity<TId> where TId : IComparable, IEquatable<TId>
 {
-    /// <summary>
-    /// <inheritdoc cref="ITimestampedEntity" />
-    /// </summary>
-    /// <typeparam name="TId"><inheritdoc cref="ITimestampedEntity{TId}"/></typeparam>
-    /// <typeparam name="TUserId">The type of the user ID</typeparam>
-    public interface ITimestampedEntity<TId, TUserId> : ITimestampedEntity<TId> where TId : IComparable, IEquatable<TId>
-    {
-        /// <value>A <see cref="ITimestamp{TUserId}"/> holding the details of the entity's creation</value>
-        new ITimestamp<TUserId> Created { get; set; }
-        /// <value>A <see cref="ITimestamp{TUserId}"/> holding the details of the entity's last update</value>
-        new ITimestamp<TUserId> Updated { get; set; }
-        /// <value>A <see cref="ITimestamp{TUserId}"/> holding the details of the entity's deletion (if applicable, <see langword="null"/>/<see langword="default"/> otherwise)</value>
-        new ITimestamp<TUserId>? Deleted { get; set; }
-    }
+    /// <value>A <see cref="ITimestamp{TUserId}"/> holding the details of the entity's creation</value>
+    new ITimestamp<TUserId> Created { get; set; }
+    /// <value>A <see cref="ITimestamp{TUserId}"/> holding the details of the entity's last update</value>
+    new ITimestamp<TUserId> Updated { get; set; }
+    /// <value>A <see cref="ITimestamp{TUserId}"/> holding the details of the entity's deletion (if applicable, <see langword="null"/>/<see langword="default"/> otherwise)</value>
+    new ITimestamp<TUserId>? Deleted { get; set; }
 }
